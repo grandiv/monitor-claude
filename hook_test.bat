36c0227b8531d6a3487839ed0e3ef05ff9440534
Grandiv <grandivfarand@gmail.com>
2026-05-22 12:52:19 +0700

    wip: snapshot pending work

---
+++ hook_test.bat
@@ -0,0 +1,2 @@
+@echo off
+echo Hook executed at %date% %time% >> C:\Projects\monitor-claude\hook_test.log
---
+++ set_working.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Python312\python.exe C:\Projects\monitor-claude\update_status.py working
